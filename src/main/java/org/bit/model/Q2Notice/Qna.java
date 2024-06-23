9c2742b4e17451efa1ff82f2163a2a13e7eaf956
donguerami <iqeq0020@gmail.com>
2024-06-23 15:29:31 +0900

    Notice 공지사항 기능구현 QnA 게시판 진행중

--- src/main/java/org/bit/model/Q2Notice/Qna.java
+++ src/main/java/org/bit/model/Q2Notice/Qna.java
@@ -4,8 +4,10 @@ import lombok.Data;
 
 @Data
 public class Qna {
-    private Long qna_id;
-    private String question;
-    private String answer;
-    private String created_at;
+    private int qna_id;         //QnA 글 번호
+    private int member_id;      //회원 아이디
+    private String qna_title;   //QnA 제목
+    private String qna_content; //QnA 내용
+    private String qna_date;    //QnA 게시글 등록 날짜
+    private String status;      //상태: QnA 글의 상태를(예: 'UNANSWERED' 또는 'ANSWERED')
 }
