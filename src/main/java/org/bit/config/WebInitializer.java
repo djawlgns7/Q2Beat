41ec5dfa403d26fc7d7cfb8b05d6754154d9f8c8
jihun <djawlgns7@gmail.com>
2024-07-02 11:03:46 +0900

    한글깨짐 수정, 포즈문제에서 사진 교환 성공

--- src/main/java/org/bit/config/WebInitializer.java
+++ src/main/java/org/bit/config/WebInitializer.java
@@ -1,8 +1,19 @@
 package org.bit.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
+
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] { characterEncodingFilter };
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
